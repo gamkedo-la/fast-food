4abbabaf5f71684d3681be679624175e2155713f
stebssbets <stebssbets@yahoo.com>
2021-10-27 11:28:47 +0200

    save name button working on tyler's menu setup

--- Assets/Scripts/Buttons/SaveButtonScript.cs
+++ Assets/Scripts/Buttons/SaveButtonScript.cs
@@ -2,14 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class SaveButtonScript : ButtonScript
 {
-    [SerializeField] InputField newPlayerNameInputField;
-    [SerializeField] Text savedNameTextField;
+    [SerializeField] TMP_InputField newPlayerNameInputField;
+    [SerializeField] TextMeshProUGUI savedNameTextField;
 
     public override void HandleButtonClick()
     {
+        Debug.Log("anything");
         if (newPlayerNameInputField.text == "")
         {
             newPlayerNameInputField.text = "You didn't enter a name";
