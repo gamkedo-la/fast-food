703b42209e6d7b27cc770d44a1dc197e3f1a3df0
stebssbets <stebssbets@yahoo.com>
2021-12-09 21:14:55 +0400

    building on itch, but not on Android (may be a storage issue on my phone)

--- Assets/StudyPhonicScript.cs
+++ Assets/StudyPhonicScript.cs
@@ -12,23 +12,26 @@ public class StudyPhonicScript : MonoBehaviour
     [SerializeField]
     PhonicObject phonicObject;
 
+    [SerializeField]
+    GameObject myParentPhonicsContainerGameObject;
+
     public string myCharacter;
     
     private AudioClip audio;
 
     private GameSoundEnum myGameSoundEnum;
 
-    private void OnValidate()
-    {
-        if (phonicObject != null)
-        {
-            myCharacter = phonicObject.character;
+    //private void OnValidate()
+    //{
+    //    if (phonicObject != null)
+    //    {
+    //        myCharacter = phonicObject.character;
 
-            audio = phonicObject.audio;
+    //        audio = phonicObject.audio;
 
-            InitializeMyGameSoundEnum();
-        }
-    }
+    //        InitializeMyGameSoundEnum();
+    //    }
+    //}
 
     private void Start()
     {
@@ -50,9 +53,28 @@ public class StudyPhonicScript : MonoBehaviour
     private void InitializeMyGameSoundEnum()
     {
         string growingString = "";
-        growingString += GameManagerScript.currentLanguage.ToString();
+
+        //growingString += GameManagerScript.currentLanguage.ToString();
+        if (myParentPhonicsContainerGameObject.name == "EnglishPhonics")
+        {
+            growingString += "English";
+        }
+        else if (myParentPhonicsContainerGameObject.name == "AlbanianPhonics")
+        {
+            growingString += "Albanian";
+        }
+        else if (myParentPhonicsContainerGameObject.name == "GeorgianPhonics")
+        {
+            growingString += "Georgian";
+        }
+
         growingString += "_Phonic_";
-        growingString += myCharacter.ToUpper();
+
+        if (myParentPhonicsContainerGameObject.name != "GeorgianPhonics")
+        {
+            growingString += myCharacter.ToUpper();
+        }
+        
         myGameSoundEnum = (GameSoundEnum)Enum.Parse(typeof(GameSoundEnum), growingString);
     }
 }
