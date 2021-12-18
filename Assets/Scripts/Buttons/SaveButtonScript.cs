504c4fb6c22bff816f5ed9dddf46c4345fbcacba
stebssbets <stebssbets@yahoo.com>
2021-12-18 17:15:32 +0400

    save button on profile creation screen has appropriate sfx

--- Assets/Scripts/Buttons/SaveButtonScript.cs
+++ Assets/Scripts/Buttons/SaveButtonScript.cs
@@ -13,10 +13,13 @@ public class SaveButtonScript : ButtonScript
     {
         if (newPlayerNameInputField.text == "")
         {
+            AudioController.instance.PlayAudio(GameSoundEnum.SFX_Incorrect_Order); 
             newPlayerNameInputField.text = "You didn't enter a name";
             return;
         }
-        
+
+        AudioController.instance.PlayAudio(GameSoundEnum.UI_Button);
+
         savedNameTextField.text = "New profile create for: " + newPlayerNameInputField.text;
         GameManagerScript.currentProfile = new ProfileDataScript(newPlayerNameInputField.text);
         
