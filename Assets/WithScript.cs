4109d73552fa75815d8008c2bf8d0cdae3bca69d
stebssbets <stebssbets@yahoo.com>
2021-12-27 18:58:00 +0400

    Back story updated to fit more finalized version of gameplay. New student study screen reformatted to teach beginning phrase and food items

---
+++ Assets/WithScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class WithScript : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI myTextMeshProUIObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (GameManagerScript.currentLanguage != Language.Georgian)
+        {
+            myTextMeshProUIObject.text = "with = " + LanguageDictionary.languageDictionary[GameManagerScript.currentLanguage]["with"];
+        }
+        else
+        {
+            myTextMeshProUIObject.text = "food item + თ, \nexample: lettuce = სალათის ფურწლი \nwith lettuce = სალათის ფურწლით";
+        }
+    }
+
+
+}
